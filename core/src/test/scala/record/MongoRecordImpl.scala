a5d44dc2e42644340de2276d8251c049b5001a16
bayurov <bajurovt@gmail.com>
2016-04-21 01:16:43 +0300

    add rogue dsl

--- core/src/test/scala/record/MongoRecordImpl.scala
+++ core/src/test/scala/record/MongoRecordImpl.scala
@@ -3,8 +3,6 @@ package record
 /**
  * Created by tim on 19.04.16.
  */
-class MongoRecordImpl extends MongoRecord {
+trait MongoRecordImpl extends MongoRecord {
   override def classAsString[C <: AnyRef](c: C):String = "{'name': 'tim', 'fio': 'bay', 'age': 23}"
 }
-
-object MongoRecordImpl extends MongoRecordImpl
