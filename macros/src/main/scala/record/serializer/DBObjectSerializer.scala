0ba520910300a846b64402954c031cbe93d83f45
bayurov <bajurovt@gmail.com>
2016-06-03 18:03:25 +0300

    ;)

--- macros/src/main/scala/record/serializer/DBObjectSerializer.scala
+++ macros/src/main/scala/record/serializer/DBObjectSerializer.scala
@@ -12,19 +12,16 @@ object DBObjectSerializer {
   case class DBObjectSerializerException(msg: String) extends RuntimeException(msg)
 
   def fromDBObject[T: TypeTag](m: DBObject):Any = fromDBObject(m, typeOf[T])
-  def fromDBObject(value: Any, tpe: Type):Any = {
-    tpe match {
-      case x if scalaz.isSimpleType(x)    => scalaz.asSimpleType(value, x)
-      case x if scalaz.isDate(x)          => scalaz.asDate(x, value)
-      case x if scalaz.isOption(x)        => scalaz.asOption(x, value)
-      case x if scalaz.isCollection(x)    => scalaz.asCollection(x, value)
-      case x =>
-        value match {
-          case y: BasicDBObject           => scalaz.asClass(x, y)
-          case _                          => throw DBObjectSerializerException(s"Error deserialize from ${tpe.typeSymbol.toString}: value $value")
-        }
-    }
-
+  def fromDBObject(value: Any, tpe: Type):Any = tpe match {
+    case x if scalaz.isSimpleType(x)    => scalaz.asSimpleType(value, x)
+    case x if scalaz.isDate(x)          => scalaz.asDate(x, value)
+    case x if scalaz.isOption(x)        => scalaz.asOption(x, value)
+    case x if scalaz.isCollection(x)    => scalaz.asCollection(x, value)
+    case x =>
+      value match {
+        case y: BasicDBObject           => scalaz.asClass(x, y)
+        case _                          => throw DBObjectSerializerException(s"Error deserialize from ${tpe.typeSymbol.toString}: value $value")
+      }
   }
 
   def asDBObject[A: TypeTag](entity: A):Any = asDBObject(entity, typeOf[A])
