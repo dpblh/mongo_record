37062acacf10a4ae134e0e17a422d906ed649a7b
bayurov <bajurovt@gmail.com>
2016-06-07 09:40:35 +0300

    fix inner class initialize

--- core/src/test/scala/record/use_cases/inner_class/InnerClassTest.scala
+++ core/src/test/scala/record/use_cases/inner_class/InnerClassTest.scala
@@ -1,5 +1,6 @@
 package record.use_cases.inner_class
 
+import record.imports._
 import record._
 /**
  * Created by tim on 25.04.16.
@@ -9,25 +10,24 @@ case class Person(id: String, email: String, personData: PersonData)
 case class PersonData(lastName: String, firstName: String)
 case class Address(street: String)
 
-class InnerClassTest extends Spec with MongoRecord {
+object Person extends Meta[Person] {
+  object id extends StringField(this)
+  object email extends StringField(this)
 
-  object person extends Meta[Person] {
-    object id extends StringField(this)
-    object email extends StringField(this)
+  object personData extends InnerField[Person, PersonData](this) {
+    object firstName extends StringField(this)
+    object lastName extends StringField(this)
 
-    object personData extends InnerField[Person, PersonData](this) {
-      object firstName extends StringField(this)
-      object lastName extends StringField(this)
-
-      object address extends InnerField[Person, Address](this) {
-        object street extends StringField(this)
-      }
+    object address extends InnerField[Person, Address](this) {
+      object street extends StringField(this)
     }
-
   }
 
+}
 
-  yes(from(person) { p =>
+class InnerClassTest extends Spec {
+
+  yes(from(Person) { p =>
     where(
         p.email === "bajurovt@gmail.com" &&
         p.personData.firstName === "tim" &&
@@ -36,7 +36,7 @@ class InnerClassTest extends Spec with MongoRecord {
   }, """db.person.find({ "$and" : [ { "$and" : [ { "email" : "bajurovt@gmail.com"} , { "person_data.first_name" : "tim"}]} , { "person_data.address.street" : "Tver"}]})""")
 
 
-  yes(from(person) { p =>
+  yes(from(Person) { p =>
     where(
       p.email === "bajurovt@gmail.com" &&
         p.personData.firstName === "tim" &&
